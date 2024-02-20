693f6ff93daeb7b930391de958cb41f54cdd7262
yinhao-2000 <2449677367@qq.com>
2024-02-20 13:36:35 +0800

    fix鉴权配置

---
+++ common/spring-security/src/main/java/com/atyinhao/security/custom/CustomMd5PasswordEncoder.java
@@ -0,0 +1,17 @@
+package com.atyinhao.security.custom;
+
+import com.atyinhao.common.utils.MD5;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomMd5PasswordEncoder implements PasswordEncoder {
+    public String encode(CharSequence rawPassword) {
+        return MD5.encrypt(rawPassword.toString());
+    }
+
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return encodedPassword.equals(MD5.encrypt(rawPassword.toString()));
+    }
+}
